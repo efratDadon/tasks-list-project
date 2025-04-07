2f5b0fc3f976d99f9c5968e5d9464532dabb2506
efratDadon <efrat7351@gmail.com>
2025-04-07 16:15:47 +0300

    API requests for tasks (GET, POST, PUT, DELETE)

---
+++ src/api/taskApi.js
@@ -0,0 +1,46 @@
+import axios from 'axios';
+
+
+const API_URL = 'http://localhost:3000/api/tasks';
+
+export const getTasks = async () => {
+  try {
+    const response = await axios.get(API_URL);
+    return response.data; 
+  } catch (error) {
+    console.error('Error fetching tasks:', error);
+    throw error; 
+  }
+};
+
+export const addTask = async (task) => {
+  try {
+    const response = await axios.post(API_URL, task);
+    return response.data;
+  } catch (error) {
+    console.error('Error adding task:', error);
+    throw error;
+  }
+};
+
+
+export const deleteTask = async (taskId) => {
+    try {
+      await axios.delete(`${API_URL}/${taskId}`);
+    } catch (error) {
+      console.error('Error deleting task:', error);
+      throw error; 
+    }
+  };
+
+export const updateTask = async (taskId, updatedTask) => {
+    try {
+      const response = await axios.put(`${API_URL}/${taskId}`, updatedTask);
+      return response.data;
+    } catch (error) {
+      console.error('Error updating task:', error);
+      throw error; 
+    }
+  };
+  
+  
